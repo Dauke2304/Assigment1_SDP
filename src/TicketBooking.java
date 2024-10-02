da6e119258083d351bca98aa862d6f11d88ef53b
20fut <231612@astanait.edu.kz>
2024-10-02 23:49:55 +0500

    fields

--- src/TicketBooking.java
+++ src/TicketBooking.java
@@ -3,6 +3,9 @@ public class TicketBooking {
     private String seatNumber;
     private String snackCombo;
     public static class TicketBookingBuilder {
+        private String movieTitle;
+        private String seatNumber;
+        private String snackCombo;
 
     }
 }
