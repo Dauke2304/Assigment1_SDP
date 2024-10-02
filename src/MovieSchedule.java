ae96239cc29a3da9596a164ca537b5f51a81d4b8
20fut <231612@astanait.edu.kz>
2024-10-03 00:05:11 +0500

    updated the movie schedule

--- src/MovieSchedule.java
+++ src/MovieSchedule.java
@@ -1,16 +1,37 @@
+// MovieSchedule Interface
 interface MovieSchedule extends Cloneable {
     MovieSchedule clone();
+    void setMovie(Movie movie);
+    Movie getMovie();
+    void setTime(String time);
+    String getTime();
 }
+
+// StandardSchedule Class
 class StandardSchedule implements MovieSchedule {
-    private String movie;
+    private Movie movie;
     private String time;
-    public StandardSchedule(String movie, String time) {
+
+    @Override
+    public void setMovie(Movie movie) {
         this.movie = movie;
-        this.time = time;
     }
+
+    @Override
+    public Movie getMovie() {
+        return this.movie;
+    }
+
+    @Override
     public void setTime(String time) {
         this.time = time;
     }
+
+    @Override
+    public String getTime() {
+        return this.time;
+    }
+
     @Override
     public MovieSchedule clone() {
         try {
