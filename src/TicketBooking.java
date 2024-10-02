58c1143a47c2db2dfa2ac91efaaf53fe54d87751
20fut <231612@astanait.edu.kz>
2024-10-02 23:49:33 +0500

    builder as a static class

--- src/TicketBooking.java
+++ src/TicketBooking.java
@@ -2,4 +2,7 @@ public class TicketBooking {
     private String movieTitle;
     private String seatNumber;
     private String snackCombo;
+    public static class TicketBookingBuilder {
+
+    }
 }
