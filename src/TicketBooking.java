43d90a2fda083993ddbc449acc4f57aeae406d75
20fut <231612@astanait.edu.kz>
2024-10-02 23:50:27 +0500

    setters

--- src/TicketBooking.java
+++ src/TicketBooking.java
@@ -6,6 +6,20 @@ public class TicketBooking {
         private String movieTitle;
         private String seatNumber;
         private String snackCombo;
+        public TicketBookingBuilder setMovieTitle(String title) {
+            this.movieTitle = title;
+            return this;
+        }
+
+        public TicketBookingBuilder setSeatNumber(String seat) {
+            this.seatNumber = seat;
+            return this;
+        }
+
+        public TicketBookingBuilder setSnackCombo(String combo) {
+            this.snackCombo = combo;
+            return this;
+        }
 
     }
 }
