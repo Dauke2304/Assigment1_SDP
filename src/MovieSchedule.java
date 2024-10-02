03db4a82cfa915c213f5e9e7627562e672c8f82a
20fut <231612@astanait.edu.kz>
2024-10-02 23:56:17 +0500

    fields

--- src/MovieSchedule.java
+++ src/MovieSchedule.java
@@ -2,5 +2,6 @@ interface MovieSchedule extends Cloneable {
     MovieSchedule clone();
 }
 class StandardSchedule implements MovieSchedule {
-
+    private String movie;
+    private String time;
 }
