c6e40ef84a90f5addf401fc0cfa6fbe06abf4dc5
20fut <231612@astanait.edu.kz>
2024-10-02 23:56:43 +0500

    constructor

--- src/MovieSchedule.java
+++ src/MovieSchedule.java
@@ -4,4 +4,8 @@ interface MovieSchedule extends Cloneable {
 class StandardSchedule implements MovieSchedule {
     private String movie;
     private String time;
+    public StandardSchedule(String movie, String time) {
+        this.movie = movie;
+        this.time = time;
+    }
 }
