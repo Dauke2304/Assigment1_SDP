d5349898ab65e043693901759cd82aca6506c090
20fut <231612@astanait.edu.kz>
2024-10-02 23:39:55 +0500

    darktheme and lighttheme classes for button, without funtion just sout's the text

--- src/Button.java
+++ src/Button.java
@@ -1,3 +1,14 @@
 interface Button {
     void render();
 }
+class DarkThemeButton implements Button {
+    public void render() {
+        System.out.println("Rendering Dark Theme Button");
+    }
+}
+
+class LightThemeButton implements Button {
+    public void render() {
+        System.out.println("Rendering Light Theme Button");
+    }
+}
