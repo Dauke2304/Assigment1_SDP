65da432ae5b94172c80dc88f8ec7a38c9af33830
20fut <231612@astanait.edu.kz>
2024-10-03 00:12:26 +0500

    updated buttons

--- src/Button.java
+++ src/Button.java
@@ -1,28 +1,23 @@
+
 interface Button {
     void render();
 }
+
+
 class DarkThemeButton implements Button {
+    @Override
     public void render() {
         System.out.println("Rendering Dark Theme Button");
     }
 }
 
-class LightThemeButton implements Button {
-    public void render() {
-        System.out.println("Rendering Light Theme Button");
-    }
-}
 interface UIFactory {
     Button createButton();
 }
+
 class DarkThemeFactory implements UIFactory {
+    @Override
     public Button createButton() {
         return new DarkThemeButton();
-    }
-}
-
-class LightThemeFactory implements UIFactory {
-    public Button createButton() {
-        return new LightThemeButton();
     }
 }
