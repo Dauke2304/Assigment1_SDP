20a3164d99d6e3e7297492e5094fd3c9d17d326a
20fut <231612@astanait.edu.kz>
2024-10-02 23:31:53 +0500

    class RegularMovie

--- src/Movie.java
+++ src/Movie.java
@@ -1,3 +1,14 @@
 interface Movie {
     String getTitle();
 }
+class RegularMovie implements Movie {
+    private String title;
+
+    public RegularMovie(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+}
